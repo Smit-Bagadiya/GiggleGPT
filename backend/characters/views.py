94755fe8a0854f7cb89cdf62bfc59703f4712817
Smit Bagadiya <smitbagadiya2005@gmail.com>
2025-07-07 20:56:22 +0530

    chore(backend): update characters/views.py

--- backend/characters/views.py
+++ backend/characters/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from .models import Character
 from .serializers import CharacterSerializer
 
 # Create your views here.
 
 class CharacterListAPIView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     
     def get(self, request):
         characters = Character.objects.all()
